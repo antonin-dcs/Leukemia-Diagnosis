35f1bb949b4d07bded017d5822f1edeedae6523b
Antonin Decouvelaere <antonin.decouvelaere@student-cs.fr>
2025-03-24 18:07:07 +0100

    fonction_appelante

---
+++ masque.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def masque(image):
+    image_np=np.array(image)
+    for k in range(np.shape(image_np)[0]):
+        for i in range(np.shape(image_np)[1]):
+            pass
+    
