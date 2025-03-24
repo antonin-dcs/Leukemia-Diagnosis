3c66bf2199b3b55d20502ef88efb3f6bb1c97e77
Antonin Decouvelaere <antonin.decouvelaere@student-cs.fr>
2025-03-24 17:32:34 +0100

    "modif resizing"

--- resising_224x224.py
+++ resising_224x224.py
@@ -1,16 +1,5 @@
 import cv2
 
-
-
-"""
-img=cv2.imread("C:\\Users\\antod\\Desktop\\Projet S6\\ia-detection-leucemie\\exemple_image.jpg")
-resized_img = cv2.resize(img, (224, 224))
-cv2.imshow('image initial', img)
-cv2.imshow('image de taille 224x224', resized_img)
-cv2.waitKey(0)
-"""
-
-
 def resizing1(image_jpg):
     """fonction qui permet de convertir la taille de l'image rentrée en paramètre au format 224 x 224
     Arguments :
@@ -20,10 +9,8 @@ def resizing1(image_jpg):
 
     img=cv2.imread(f"C:\\Users\\antod\\Desktop\\Projet S6\\ia-detection-leucemie\\{image_jpg}")
     resized_img = cv2.resize(img, (224, 224))
-    cv2.imshow('image initial', img)
-    cv2.imshow('image de taille 224x224', resized_img)
     cv2.waitKey(0)
     return
 
 
-resizing1("exemple_image.jpg")
+#resizing1("exemple_image.jpg")
