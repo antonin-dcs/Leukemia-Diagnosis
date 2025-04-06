58c198f1f4a5001297aa364142e5e61aa0e40c8d
Antonin Decouvelaere <antonin.decouvelaere@student-cs.fr>
2025-04-06 19:29:40 +0200

    ajout d'un masque

--- masque.py
+++ masque.py
@@ -1,18 +1,10 @@
+import cv2 as cv
 
-import numpy as np
-import cv2
+def remove_isolated_pixels(img,k=7):
+    """k est la taile du motif pour supprimer le bruit avec la focntion otsu"""
+    blur = cv.GaussianBlur(img,(k,k),0)
+    ret3,img_th3 = cv.threshold(blur,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+    return img_th3
 
-def remove_isolated_pixels(src):
-    kernel = np.ones((3, 3), np.uint8)  # Kernel 3x3 pour compter les voisins
-    neighbor_count = cv2.filter2D(src, ddepth=-1, kernel=kernel)  # Comptage des pixels blancs autour de chaque pixel
+
     
-    # Masques pour les pixels isolés
-    isolated_white = (src == 255) & (neighbor_count <= 255 * 2)  # Pixels blancs isolés
-    isolated_black = (src == 0) & (neighbor_count >= 255 * 7)    # Pixels noirs isolés
-    
-    # Création d'une copie et correction des pixels isolés
-    result = src.copy()
-    result[isolated_white] = 0  # Remplace les pixels blancs isolés par du noir
-    result[isolated_black] = 255  # Remplace les pixels noirs isolés par du blanc
-    
-    return result
